3c9ec3764b48095196660d398e932ec39c52854f
Anton <anton.d.shilin@gmail.com>
2022-08-23 22:10:27 +0300

    fix
    added answer test column, added fk for questions

--- TestGuru/db/migrate/20220823190838_create_answers.rb
+++ TestGuru/db/migrate/20220823190838_create_answers.rb
@@ -2,6 +2,9 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.boolean :correct, null: false, default: false
+      t.text :answer
+      t.references :question, foreign_key: { to_table: 'questions' }
+
 
       t.timestamps
     end
--- TestGuru/db/schema.rb
+++ TestGuru/db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_111014) do
+ActiveRecord::Schema.define(version: 2022_08_23_190838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
+    t.text "answer"
+    t.bigint "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -52,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_08_23_111014) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
